85e05789e73f236b19b7a876939a1f282461edb1
Sarah <70615195+smanter82@users.noreply.github.com>
2020-12-16 15:58:07 -0500

    I got the delete request to workgit add .  Woohoogit add .!

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,8 +8,6 @@ module.exports = function(app) {
     //API GET Request
 
     app.get('/api/notes', function (req, res) {
-      console.log("api get route");
-      // fs.readFileSync("../db/db.json");
       res.json(db);
 
     });
@@ -33,21 +31,24 @@ module.exports = function(app) {
 });
 
 //API delete request
-app.delete('/api/notes', function(req, res) {
+app.delete('/api/notes/:id', function(req, res) {
   //find note by id and delete it.  
-  const noteId = parseInt(req.params.id);
+  const noteId = req.params.id;
     
-    for (i=0; i<db.length; i++) {
-      if (db[i].id == noteId){
-        res.send(db[i]);
-        db.splice(i, 1);
-      }
+  for (let i=0; i<db.length; i++) {
+    if (db[i].id == noteId){
+      db.splice(i, 1);
     }
-    fs.writeFile(path.join(__dirname, "../db/db.json"), JSON.stringify(db), (err)=>{
-      if(err) throw error;
-    });  
-    res.json(db)
+  }
+  fs.writeFile(path.join(__dirname, "/../db/db.json"), JSON.stringify(db), (err)=>{
+    if(err) throw error;
+  });
+  
+  res.send(db);  
 
-})
+
+   
+
+});
 
 }
